48a7e69cf8db7169dd5ec3f7f49e9aeafc278135
Mahmoud Alkhraishi <mkhraisha@hotmail.com>
2023-08-30 13:49:53 -0400

    upgrade soroban client

--- src/config.ts
+++ src/config.ts
@@ -5,25 +5,25 @@ const networks = [
     id: 0,
     url: 'https://horizon.stellar.org',
     passPhrase: 'Public Global Stellar Network ; September 2015',
-    contractId: 'bfe3a307860e15409c7209b5acf81205fec3d571967fa163f820c5d2326575e5'
+    contractId: 'CCAPJPN6WNLJ7ZPPZLZZLLLQUVB2MDLRHXAXPVHQOF5DYWFISBCFSKZD'
   },
   {
     id: 1,
     url: 'https://horizon-testnet.stellar.org',
     passPhrase: 'Test SDF Network ; September 2015',
-    contractId: 'bfe3a307860e15409c7209b5acf81205fec3d571967fa163f820c5d2326575e5'
+    contractId: 'CCAPJPN6WNLJ7ZPPZLZZLLLQUVB2MDLRHXAXPVHQOF5DYWFISBCFSKZD'
   },
   {
     id: 2,
     url: 'https://rpc-futurenet.stellar.org',
     passPhrase: 'Test SDF Future Network ; October 2022',
-    contractId: 'bfe3a307860e15409c7209b5acf81205fec3d571967fa163f820c5d2326575e5'
+    contractId: 'CA2F3IPU2INVPNFUWLYLKKLXX65LX6FHU4J3U2NKENR2NW4RSIBUM4C2'
   },
   {
     id: 3,
     url: 'http://localhost:8000/soroban/rpc',
     passPhrase: 'Standalone Network ; February 2017',
-    contractId: '4681247678bb9dc0739e5d97c2aa91c2a4f9efc0e59751cc02547bdbafb2c9c3'
+    contractId: 'CA27WL6LYG2S73G2ESG3CNLCFEC23QNXN2GWZPEMJBPKTVYYVAY3GURK'
   }
 ]
 
